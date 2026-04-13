20646135fd95bbf69a867303bf8ee04505cb30e4
RitikY2005 <ritikeshyadav2020@gmail.com>
2026-04-13 14:36:58 +0530

    SETUP: frontend/backend project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='text-lg bg-red-600 flex justify-center item-center w-screen h-screen'>
+      real estate website 
+    </div>
+  )
+}
+
+export default App
